bbd634b6e5a55c466fc478ad9fbbb9f74aa364da
Leonardo Bazico <leonardobazico@gmail.com>
2023-10-24 14:36:23 -0400

    Include untracked files in the pre commit stash

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -4,7 +4,7 @@ set -e
 STASH_NAME=pre-commit-$(date +%s)
 
 echo "Stashing unstaged changes"
-git stash save --quiet --keep-index $STASH_NAME
+git stash save --quiet --keep-index --include-untracked $STASH_NAME
 
 function restoreStashedIfAny {
   STASH_NUM=$(git stash list | grep $STASH_NAME | sed -re 's/stash@\{(.*)\}.*/\1/')
